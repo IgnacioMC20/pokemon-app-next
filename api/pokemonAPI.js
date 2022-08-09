9aa0e9725c479802354cc2c30e88281a450d73e1
Ignacio Cuyun <ignaciocuyun@MBGTITOA68A.local>
2022-08-09 13:20:17 -0600

    9 august - SSG PokemonApp

---
+++ api/pokemonAPI.js
@@ -0,0 +1,34 @@
+
+
+const baseURL = 'https://pokeapi.co/api/v2';
+
+export const getPokemons = async () => {
+    const url = `${baseURL}/pokemon?limit=151`;
+    const response = await fetch(url);
+    const pokemons = await response.json();
+
+    pokemons.results.forEach((poke, i) => {
+        poke.id = i + 1;
+        poke.image = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${i + 1}.png`;
+    })
+
+    return pokemons;
+}
+
+export const getPokemon = async (id) => {
+    const url = `${baseURL}/pokemon/${id}`;
+    const response = await fetch(url);
+    const pokemon = await response.json();
+    return {
+        name: pokemon.name,
+        sprites: pokemon.sprites,
+        image: pokemon.sprites.other.dream_world.front_default,
+        id
+    }
+}
+
+export const getNames = async() => {
+  const data = await fetch(`${baseURL}/pokemon?limit=151`);
+  const { results } = await data.json();
+    return results.map(({ name }) => name);
+}
